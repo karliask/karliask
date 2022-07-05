3d227a8bb88bbfde3ae92782b87d5af4d97c2c6d
karliask <70177702+karliask@users.noreply.github.com>
2022-07-05 19:59:20 -0300

    Add files via upload

--- choice/script.js
+++ choice/script.js
@@ -1,19 +1,20 @@
 const resultLabel = document.getElementById('resultLabel');
-let first = 1
 let second = 2
 let counter = 0
-let num = [first, second]
 let type1 = "Si"
 let type2 = "No"
 let button = document.getElementById('change');
 
 function Randomizer(){
     let firstRes = Math.floor(Math.random() * 2 + 1)
-    let secondRes = Math.floor(Math.random() * 2 + 1)
-    let thirdRes = Math.floor(Math.random() * 2 + 1)
-    console.log(firstRes, secondRes, thirdRes)
+    resultLabel.animate([
+        {opacity: 0, marginBottom: "20px"},
+        {opacity: 1, marginBottom: 0}
+    ], {duration: 400})
+    
+    console.log(firstRes)
 
-    if(firstRes == 2 && secondRes == 2 || firstRes == 2 && thirdRes == 2 || secondRes == 2 && thirdRes == 2 || firstRes == 2 && secondRes == 2 && thirdRes == 2){
+    if(firstRes == 1){
     {
         resultLabel.innerHTML = type1
     }
