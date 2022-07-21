8b27106019062771b7a40a54e18fd0caa7928fef
karliask <70177702+karliask@users.noreply.github.com>
2022-07-21 16:18:54 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-function createKarliask() { 
-    var karliask = new Person();
-
-    karliask.setName("karliask");
-    karliask.setBio("dev @seesonss, dev @Peereira7 mod @ Team Heretics");
-    karliask.setNicknames(["karliask", "karl"]);
-    karliask.setLocation("Ciudad Autonoma de Buenos Aires, Argentina");
-
-    /* spending my time on */
-    karliask.addActivity({ name: "100DaysOfCode", link: "https://discord.gg/peereira7" });
-    karliask.addActivity({ name: "Personal Projects"});
-    karliask.addActivity({ name: "Team Heretics", role: "mod", link: "https://discord.gg/teamheretics" });
-    karliask.addJob({ name: "seesonss", role: "dev", link: "https://discord.gg/seesonss" });
-    karliask.addJob({ name: "danoninomb.es", role: "dev", link: "https://danoninomb.es" });
-
-    /* contact me on */
-    karliask.addContact({ name: "discord", link: "https://twitter.com/danonino_mb" });
-    karliask.addContact({ name: "steam", link: "https://discord.gg/user/danonino.mb ツ" });
-    karliask.addContact({ name: "instagram", link: "https://instagram.com/danonino.mb" });
-    karliask.addContact({ name: "twitter", link: "https://github.com/danonino-mb" });
-}
-
-// Make the DIV element draggable:
-
 const terminal = document.getElementById("terminal");
 const body = document.querySelector("body");
 const a = document.querySelector("a");
@@ -60,5 +36,4 @@ function log2(){
           }
         }, 1000);
     
-    console.log("funca2")
 }
