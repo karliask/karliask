22ccac08c10770cfe27ac5575c164d099f212a83
karliask <70177702+karliask@users.noreply.github.com>
2022-09-11 20:10:20 -0300

    Add files via upload

--- weather/main.js
+++ weather/main.js
@@ -11,14 +11,13 @@ const kelvin = 273;
 window.addEventListener("load", () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
       lon = position.coords.longitude;
       lat = position.coords.latitude;
   
       const api = "6d055e39ee237af35ca066f35474e9df";
   
       const base =
-`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
+`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
 `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
   
       fetch(base)
@@ -26,7 +25,6 @@ window.addEventListener("load", () => {
           return response.json();
         })
         .then((data) => {
-          console.log(data);
           temperature.textContent = 
               "Temperature: " + Math.floor(data.main.temp - kelvin) + "°C"; 
           feelsLike.textContent = 
