c88fe1ec4fddf0d9518c3521d3124aad7d8bab6c
karliask <70177702+karliask@users.noreply.github.com>
2022-09-12 21:31:26 -0300

    Add files via upload

--- weather/main.js
+++ weather/main.js
@@ -7,6 +7,37 @@ let humidity = document.querySelector(".humidity")
 let loc = document.querySelector(".location");
 let icon = document.querySelector(".icon");
 const kelvin = 273;
+var isInEnglish = true;
+var languageLabel = document.getElementById('languageLabel')
+
+var arrTemp = ['Temperature: ', 'Temperatura: ']
+var arrFL = ['Feels Like: ', 'ST: ']
+var arrHum = ['Humidity: ', 'Humedad: ']
+var arrDesc = []
+
+var temp = arrTemp[0];
+var FL = arrFL[0];
+var Hum = arrHum[0];
+var Desc = arrDesc[0];
+
+languageLabel.addEventListener('click', () => {
+  if(isInEnglish === true){
+    temp = arrTemp[1];
+    FL = arrFL[1];
+    Hum = arrHum[1];
+    Desc = arrDesc[1];
+    languageLabel.innerHTML = 'EN'
+    isInEnglish = false
+  }
+  else{
+    temp = arrTemp[0];
+    FL = arrFL[0];
+    Hum = arrHum[0];
+    Desc = arrDesc[0];
+    languageLabel.innerHTML = 'ES'
+    isInEnglish = true
+  }
+})
   
 window.addEventListener("load", () => {
   if (navigator.geolocation) {
@@ -26,23 +57,35 @@ window.addEventListener("load", () => {
         })
         .then((data) => {
           temperature.textContent = 
-              "Temperature: " + Math.floor(data.main.temp - kelvin) + "°C"; 
+              temp + Math.floor(data.main.temp - kelvin) + "°C"; 
           feelsLike.textContent = 
-              "Feels Like: " + Math.floor(data.main.feels_like - kelvin) + "°C"; 
-          humidity.textContent = "Humidity: " + data.main.humidity + "%";
+              FL + Math.floor(data.main.feels_like - kelvin) + "°C"; 
+          humidity.textContent = Hum + data.main.humidity + "%";
               summary.textContent = data.weather[0].description;
           loc.textContent = data.name;
           if(data.weather[0].description == "clear sky"){
             icon.classList.add('fa-sun')
           }
-          else if(data.weather[0].description == "light rain"){
+          else if(data.weather[0].description == "few clouds" || data.weather[0].description == "scattered clouds"){
+            icon.classList.add('fa-cloud-sun')
+          }
+          else if(data.weather[0].description == "broken clouds"){
+            icon.classList.add('fa-clouds')
+          }
+          else if(data.weather[0].description == "light rain" || data.weather[0].description == "rain"){
             icon.classList.add('fa-cloud-showers')
           }
           else if(data.weather[0].description == "moderate rain"){
             icon.classList.add('fa-cloud-showers-heavy')
           }
+          else if(data.weather[0].description == "thunderstorm"){
+            icon.classList.add('fa-cloud-bolt')
+          }
           else if(data.weather[0].description == "snow"){
             icon.classList.add('fa-cloud-snow')
+          }
+          else if(data.weather[0].description == "mist"){
+            icon.classList.add('fa-cloud-fog')
           }
           else if(data.weather[0].description == "clouds"){
             icon.classList.add('fa-cloud')
