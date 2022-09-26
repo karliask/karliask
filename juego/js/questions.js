91894af68f02ef6cd67f53aeefff5888d8eb57a1
karliask <70177702+karliask@users.noreply.github.com>
2022-09-25 23:45:16 -0300

    Add files via upload

---
+++ juego/js/questions.js
@@ -0,0 +1,193 @@
+let questions = [
+    {
+    numb: 1,
+    question: "¿Hay racismo hacia los blancos?",
+    answer: "No",
+    options: [
+      "Si",
+      "Depende de la situacion",
+      'No',
+      'No se'
+    ]
+  },
+    {
+    numb: 2,
+    question: "¿Es correcto emplear el termino raza para diferenciar a personas o grupos?",
+    answer: "No",
+    options: [
+      "Si",
+      "Situacionalmente",
+      "No",
+      "Tal vez"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Cuando es el dia del odio según la ONU?",
+    answer: "18 de Junio",
+    options: [
+      "17 de Julio",
+      "15 de Agosto",
+      "14 de Marzo",
+      "18 de Junio"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Que es el racismo?",
+    answer: 'Pensamiento que defiende la superioridad de una “raza” frente a las demas',
+    options: [
+      'Pensamiento que defiende la superioridad de una “raza” frente a las demas',
+      "Que no te guste el color negro",
+      'Odio o rechazo hacia las personas de otros paises',
+      "Odio hacia la gente con distinto color de piel"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Si uso negro como insulto, es racista?",
+    answer: "Si",
+    options: [
+      "Si",
+      "No, pero solo si es en joda",
+      "No",
+      "Depende del contexto"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "¿Que significan las siglas LGBT?",
+    answer: "Lesbian Gay Bisexual Transexual",
+    options: [
+      "Lesbian Gay Bisexual Transexual",
+      "Love Grace Beauty Trend",
+      "No se",
+      "Nada"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "¿Es la identidad es estática? ",
+    answer: "No",
+    options: [
+      "Si",
+      "A veces",
+      "No",
+      "Varia segun la persona"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "La cultura moldea la identidad de una persona",
+    answer: "Verdadero",
+    options: [
+      "Verdadero",
+      "Verdadero, con excepciones",
+      "Falso",
+      "No se"
+    ]
+  },
+  {
+    numb: 8,
+    question: "¿Que es la identidad?",
+    answer: "Conjunto de rasgos o características de una persona o cosa que permiten distinguirla de otras en un conjunto",
+    options: [
+      "Lo que nos define como personas",
+      "No tiene un significado como tal, varia segun la persona",
+      "Son muchas cosas",
+      "Conjunto de rasgos o características de una persona o cosa que permiten distinguirla de otras en un conjunto"
+    ]
+  },
+  {
+    numb: 9,
+    question: "¿Hay pocas culturas?",
+    answer: "Para nada",
+    options: [
+      "Si",
+      "Es relativo",
+      "Para nada",
+      "No se"
+    ]
+  },
+  {
+    numb: 10,
+    question: "¿Son la musica o comida parte de una cultura?",
+    answer: "Si",
+    options: [
+      "Si",
+      "Si, pero solo una de las dos",
+      "No",
+      "No se"
+    ]
+  },
+  {
+    numb: 11,
+    question: "La cultura varia dependiendo de la sociedad en la que uno este",
+    answer: "Verdadero",
+    options: [
+      "Verdadero",
+      "Verdadero, pero hay excepciones",
+      "Falso",
+      "No se"
+    ]
+  },  
+  {
+    numb: 12,
+    question: "Una cultura puede ser superior a otra",
+    answer: "Falso",
+    options: [
+      "Verdadero",
+      "Depende de que cultura estemos hablando",
+      "Falso",
+      "Puede ser"
+    ]
+  },
+  {
+    numb: 13,
+    question: "¿Que es la cultura?",
+    answer: "Conjunto de conocimientos, ideas, tradiciones y costumbres que caracterizan a un pueblo, a una clase social, a una época, etc",
+    options: [
+      "Es muy abstracto para que tenga una deficion",
+      "Son muchas cosas, demasiadas para poder definirlas",
+      "Conjunto de conocimientos, ideas, tradiciones y costumbres que caracterizan a un pueblo, a una clase social, a una época, etc",
+      "Son ideas y cosas que comparte un grupo de personas"
+    ]
+  },
+  {
+    numb: 14,
+    question: "¿Está bien excluir gente de otros países o sentir odio hacia otros países solo por su cultura?",
+    answer: "No",
+    options: [
+      "Si",
+      "Depende del país y persona",
+      "No",
+      "No se"
+    ]
+  },  
+  {
+    numb: 15,
+    question: "¿Es correcto decir que los ritos de otras sociedades están mal solo porque en tu cultura es erróneo? ",
+    answer: "No",
+    options: [
+      "Obvio",
+      "Depende del rito",
+      "No",
+      "Tal vez"
+    ]
+  },  
+  {
+    numb: 16,
+    question: "¿Que es la xenofobia? ",
+    answer: "Es el rechazo u odio al extranjero o inmigrante",
+    options: [
+      "Odio a la gente diferente",
+      "Es el rechazo u odio al extranjero o inmigrante",
+      "Odio a la gente con distinto color de piel",
+      "No se"
+    ]
+  },
+  
+];
